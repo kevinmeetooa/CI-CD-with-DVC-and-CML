011779015a43d6fa443badfab5ee20975af2a836
Kevin MEETOOA <47213487+kevinmeetooa@users.noreply.github.com>
2023-03-26 18:27:06 +0200

    Update train.py

--- train.py
+++ train.py
@@ -34,7 +34,7 @@ model = tf.keras.models.Sequential([
                                input_shape=(28, 28, 1)),
         tf.keras.layers.MaxPooling2D((2, 2)),
         tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(256, activation='relu'),
+        tf.keras.layers.Dense(40, activation='relu'),
         tf.keras.layers.Dropout(0.5),
         tf.keras.layers.Dense(10, activation='softmax')
 ])
